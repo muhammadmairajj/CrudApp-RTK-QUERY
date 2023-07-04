f1742e697764084c1b9a6438b92000338f6e3d5f
Muhammad-Mairaj <muhammadmairajj@gmail.com>
2023-07-04 16:15:14 +0500

    build folder upload

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { Provider } from "react-redux";
-import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
-import { apiSlice } from './features/api/apiSlice.ts';
-import { photoSlice } from './features/photoapi/photoSlice.ts';
 import { store } from './app/store.ts';
 import './index.css'
 
